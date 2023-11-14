75e01180c03ecbaab7ffb7b74b5af827bdcde181
ALIPHATIC <adegokefuad1@gmail.com>
2023-11-14 23:25:54 +0100

    Begining of Design Implementation

--- slidescribe/src/components/pages/Home/HomePage.tsx
+++ slidescribe/src/components/pages/Home/HomePage.tsx
@@ -1,14 +1,9 @@
-
-// import ContactSection from './ContactSection';
-
-
+import Navigation from "./Navigation";
 
 export default function HomePage() {
     return (
         <div>
-        <h1>
-            Home Page
-        </h1>
+            <Navigation />
         </div>
     );
 }
--- slidescribe/src/main.tsx
+++ slidescribe/src/main.tsx
@@ -3,27 +3,35 @@ import ReactDOM from 'react-dom';
 import './styles/tailwind.css';
 import './constants/apiEndpoints';
 import './constants/route';
-
+import App from './App';
+import HomePage from './components/pages/Home/HomePage';
 // import authContext from './context/AuthContext';
 // import useFetch from './hooks/useFetch';
 // import apiService from './services/apiService';
 // import formatDate from './utils/formatDate';
 // import homeView from './views/HomeView';
 
-import AboutPage from './components/pages/About/AboutPage';
-import AuthPage from './components/pages/Auth/AuthPage';
-import DashboardPage from './components/pages/Dashboard/DashboardPage';
-import HomePage from './components/pages/Home/HomePage';
+// import AboutPage from './components/pages/About/AboutPage';
+// import AuthPage from './components/pages/Auth/AuthPage';
+// import DashboardPage from './components/pages/Dashboard/DashboardPage';
+// import HomePage from './components/pages/Home/HomePage';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <HomePage />
-      <AboutPage />
-      <AuthPage />
-      <DashboardPage />
-    </div>
-  );
-};
+// const App: React.FC = () => {
+//   return (
+//     <div>
+//       <HomePage />
+//       <AboutPage />
+//       <AuthPage />
+//       <DashboardPage />
+//     </div>
+//   );
+// };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <HomePage />
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
