80e8f854c5290951112e0ee498c4c0fbd65b0387
ALIPHATIC <adegokefuad1@gmail.com>
2023-11-22 08:00:52 +0100

    Restructuring Files into their Parent Folders

--- slidescribe/src/App.tsx
+++ slidescribe/src/App.tsx
@@ -1,15 +1,13 @@
-import Header from "./components/organisms/Header";
 import AboutPage from "./components/pages/About/AboutPage";
-import SignUpAuthPage from "./components/pages/Auth/SignUpAuthPage";
-// import LoginAuthPage from "./components/pages/Auth/LoginAuthPage";
+import AuthPage from "./components/pages/Auth/AuthPage";
+import HomePage from "./components/pages/Home/HomePage";
 
 function App() {
   return (
     <div className="max-h-screen">
-      <Header />
-      <SignUpAuthPage />
+      <HomePage />
+      <AuthPage />
       <AboutPage />
-      {/* <LoginAuthPage /> */}
     </div>
   );
 }
---
+++ slidescribe/src/components/pages/Auth/AuthPage.tsx
@@ -0,0 +1,15 @@
+import SignUpAuthPage from "./SignUpAuthPage";
+// import LoginAuthPage from "./components/pages/Auth/LoginAuthPage";
+
+
+
+const AuthPage = () => {
+    return (
+        <div>
+        <SignUpAuthPage />
+        {/* <LoginAuthPage /> */}
+        </div>
+    );
+    };
+
+export default AuthPage;
