315a3c21a8409e642b967cf01de88b7bdf39a8f7
semako123 <zosusemenoch@gmail.com>
2023-11-24 22:46:06 +0100

    Integrate Authentication service class
    -Implement Auth service methods such as login, signup and logout
    -Add room for error handling.

---
+++ slidescribe/src/services/authService.ts
@@ -0,0 +1,72 @@
+import { AuthUserData } from "./@types/authTypes";
+
+class AuthService {
+	private apiUrl: string;
+	constructor(apiUrl: string) {
+		this.apiUrl = apiUrl;
+	}
+
+	async login(userData: AuthUserData) {
+		try {
+			const response = await fetch(`${this.apiUrl}`, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(userData),
+			});
+
+			if (!response.ok) {
+				throw new Error("Login Failed");
+			}
+
+			const data = await response.json();
+			return data; //Returns token authorization
+		} catch (err) {
+			throw new Error("Login Failed");
+		}
+	}
+
+	async signup(userData: AuthUserData) {
+		try {
+			const response = await fetch(`${this.apiUrl}`, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(userData),
+			});
+
+			if (!response.ok) {
+				throw new Error("Signup Failed");
+			}
+
+			const data = await response.json();
+			return data; //Returns token for authorization
+		} catch (err) {
+			throw new Error("Login Failed");
+		}
+	}
+
+	async logout(token: string) {
+		try {
+			const response = await fetch(`${this.apiUrl}`, {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({token}),
+			});
+
+			if (!response.ok) {
+				throw new Error("Logout Failed");
+			}
+
+			return response.ok;
+		} catch (err) {
+			throw new Error("Logout Failed");
+		}
+	}
+}
+
+export default AuthService;
