35bff3d7697cd9b8e767a02dea8532ad910f77e9
Semako123 <zosusemenoch@gmail.com>
2023-12-16 02:36:18 +0100

    update button to accomodate type\varaiant prop

--- slidescribe/src/components/organisms/Buttons.tsx
+++ slidescribe/src/components/organisms/Buttons.tsx
@@ -1,36 +1,46 @@
 import React from "react";
 
-type CustomButtonType = "primary" | "secondary" | "tertiary";
+type CustomButtonVariant = "primary" | "secondary" | "tertiary";
+type CustomButtonType = "button" | "submit" | "reset" | undefined;
 
 type ButtonProps = {
-  type: CustomButtonType;
-  children: React.ReactNode;
-  onClick?: () => void;
-  styleHolder?: string;
+	variant: CustomButtonVariant;
+	children: React.ReactNode;
+	onClick?: () => void;
+	styleHolder?: string;
+	type?: CustomButtonType;
+	disabled?: boolean | undefined;
 };
 
 const Button: React.FC<ButtonProps> = ({
-  type,
-  children,
-  onClick,
-  styleHolder,
+	variant,
+	type,
+	children,
+	onClick,
+	styleHolder,
+	disabled,
 }) => {
-  const buttonClasses = `px-3 py-2 rounded text-sm font-medium cursor-pointer w-full ${
-    type === "primary"
-      ? "bg-purple-300 border-solid border border-purple-400 text-neutral-0 hover:bg-purple-500"
-      : type === "secondary"
-      ? "bg-purple-50 border-solid border border-purple-400 text-purple-400 hover:bg-purple-500 hover:text-neutral-0"
-      : "bg-neutral-0 border-solid border-neutral-0 text-purple-400 hover:bg-purple-50 hover:border-purple-50"
-  }  
+	const buttonClasses = `px-3 py-2 rounded text-sm font-medium cursor-pointer w-full ${
+		variant === "primary"
+			? "bg-purple-300 border-solid border border-purple-400 text-neutral-0 hover:bg-purple-500"
+			: variant === "secondary"
+			? "bg-purple-50 border-solid border border-purple-400 text-purple-400 hover:bg-purple-500 hover:text-neutral-0"
+			: "bg-neutral-0 border-solid border-neutral-0 text-purple-400 hover:bg-purple-50 hover:border-purple-50"
+	}  
   ${styleHolder}`;
 
-  return (
-    <div>
-      <button className={buttonClasses} type="button" onClick={onClick}>
-        {children}
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<button
+				className={buttonClasses}
+				type={type}
+				onClick={onClick}
+				disabled={disabled}
+			>
+				{children}
+			</button>
+		</div>
+	);
 };
 
 export default Button;
