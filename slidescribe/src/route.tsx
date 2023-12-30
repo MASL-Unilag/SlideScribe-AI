3ee87d27864fe19b92acfcf318916b62e1395e7a
semako123 <zosusemenoch@gmail.com>
2023-12-30 05:36:33 +0100

    Update routes

--- slidescribe/src/route.tsx
+++ slidescribe/src/route.tsx
@@ -1,22 +1,30 @@
-import { useRoutes } from 'react-router-dom';
-import HomePage from './components/pages/Home/HomePage';
-import AboutPage from './components/pages/About/AboutPage';
-import SignUpAuthPage from './components/pages/Auth/SignUpAuthPage';
-import LoginAuthPage from './components/pages/Auth/LoginAuthPage';
-import AccountPage from './components/pages/Account/AccountPage';
+import { useRoutes } from "react-router-dom";
+import HomePage from "./components/pages/Home/HomePage";
+import AboutPage from "./components/pages/About/AboutPage";
+import SignUpAuthPage from "./components/pages/Auth/SignUpAuthPage";
+import LoginAuthPage from "./components/pages/Auth/LoginAuthPage";
+import AccountPage from "./components/pages/Account/AccountPage";
+import Sidebar from "./components/organisms/Sidebar";
 
-const Routes = () => { 
-  const element = useRoutes([
+const Routes = () => {
+	const element = useRoutes([
 		{ path: "/", element: <HomePage /> },
+		{
+			path: "/dashboard",
+			element: <Sidebar />,
+			children: [
+				{ path: "", element: <div>Dash</div> },
+				{ path: "account", element: <AccountPage /> },
+				{ path: "*", element: <div>404 not found</div> },
+			],
+		},
 		{ path: "/about", element: <AboutPage /> },
 		{ path: "/signup", element: <SignUpAuthPage /> },
 		{ path: "/login", element: <LoginAuthPage /> },
-		{ path: "/account", element: <AccountPage /> },
 		{ path: "*", element: <div>Not Found</div> },
-  ]);
+	]);
 
-  return element;
-}
+	return element;
+};
 
 export default Routes;
-
