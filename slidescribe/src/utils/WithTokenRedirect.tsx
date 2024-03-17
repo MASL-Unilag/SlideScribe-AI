5966d3d745a3ef585a3d2b668d9d451f2827b896
semako123 <zosusemenoch@gmail.com>
2024-03-17 06:54:32 -0700

    Fixes referesh time

--- slidescribe/src/utils/WithTokenRedirect.tsx
+++ slidescribe/src/utils/WithTokenRedirect.tsx
@@ -1,19 +1,19 @@
-import {ReactNode, useEffect} from "react";
+import { ReactNode, useEffect } from "react";
 import useAuth from "../hooks/useAuth";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export const WithTokenRedirect = ({children}: { children: ReactNode }) => {
-    const navigate = useNavigate();
-    const {isAuthenticated} = useAuth();
-    useEffect(() => {
-        if (!isAuthenticated) {
-            const timerId = setTimeout(() => {
-                navigate("/login");
-            }, 10);
+export const WithTokenRedirect = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const timerId = setTimeout(() => {
+        navigate("/login");
+      }, 100);
 
-            return () => clearTimeout(timerId);
-        }
-    }, [isAuthenticated, navigate]);
+      return () => clearTimeout(timerId);
+    }
+  }, [isAuthenticated, navigate]);
 
-    return children;
+  return children;
 };
