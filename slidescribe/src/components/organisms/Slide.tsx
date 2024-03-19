eca7a1d5452b205e37465ebfbfd1cb6a6d3872e9
semako123 <zosusemenoch@gmail.com>
2024-03-19 04:08:56 -0700

    Update slides listing component

--- slidescribe/src/components/organisms/Slide.tsx
+++ slidescribe/src/components/organisms/Slide.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import Overlays from "./Overlays";
 import { PresentationPreview } from "./PresentationPreview";
 
-export default function Slide() {
-  const istring = "Introduction to health and safety maintenance";
-  const idate = "2024-01-16T15:25:50.823Z";
-  const url =
-    "https://scribeslides.blob.core.windows.net/scribecontainer/Adobe%20Photoshop%20Tutorial.pptx";
+export interface SlideProps {
+  title: string;
+  date: string;
+  url: string;
+}
+
+export default function Slide({slideInfo}:{slideInfo:SlideProps}) {
   const [openPreview, setOpenPreview] = useState<boolean>(false);
 
   const formatStr = (str: string): string => {
@@ -42,12 +44,12 @@ export default function Slide() {
           <img src={thumbnail} alt="slides" className="w-full" />
         </div>
         <div className="p-4 text-sm text-neutral-400">
-          <p className="font-medium">{formatStr(istring)}</p>
-          <p className="text-xs mt-2">{formatDate(idate)}</p>
+          <p className="font-medium">{formatStr(slideInfo.title)}</p>
+          <p className="text-xs mt-2">{formatDate(slideInfo.date)}</p>
         </div>
       </div>
       <Overlays
-        children={<PresentationPreview url={url} close={toggleClose} />}
+        children={<PresentationPreview url={slideInfo.url} close={toggleClose} />}
         isOpen={openPreview}
       />
     </>
